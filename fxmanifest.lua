ffef693221a46a30a2a13e383abbb7d76dc3100e
Snnaples <68153702+Snnaples@users.noreply.github.com>
2022-10-29 01:38:24 +0300

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,8 @@
-local metadata = {fx = 'cerulean', game = 'gta5', server_script = 'scheduler.lua'}
+local metadata = {fx = 'cerulean', game = 'gta5', scripts = {'@vrp/lib/utils.lua','scheduler.lua'}}
 
 fx_version(metadata.fx)
 game(metadata.game)
+server_scripts(metadata.scripts)
 description [[
   Callback scheduler
   v1.0
