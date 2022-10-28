bec46bb8b8ac009d1cf395a0f4a2543da1a455d1
Snnaples <68153702+Snnaples@users.noreply.github.com>
2022-10-29 01:36:49 +0300

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+local metadata = {fx = 'cerulean', game = 'gta5', server_script = 'scheduler.lua'}
+
+fx_version(metadata.fx)
+game(metadata.game)
+description [[
+  Callback scheduler
+  v1.0
+]]
+
+server_only (1)
+
+author 'Snnaples'
+
+
+
